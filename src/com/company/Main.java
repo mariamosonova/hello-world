8d8305d7ab1cd7348feba84e17e5ed803f0dca8e
Sokolova Maria <mariamosonova28@gmail.com>
2020-01-29 01:29:29 +0300

    Hello world app.

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String h = "Hello";
+        String w = "World";
+        System.out.println(h+" "+w+"!");
+
+//        int a = 5;
+//        int b = 6;
+//        int c = a + b;
+//        System.out.println("a>b "+(a>b));
+//        System.out.println("a<b "+(a<b));
+//        int i = 1;
+//        System.out.println(i);
+//        i = i + 1;
+//        System.out.println(i);
+//        i = i + 1;
+//        System.out.println("1...");
+//        System.out.println("2...");
+//        System.out.println("3...");
+//        System.out.println("Hello World!");
+    }
+}
